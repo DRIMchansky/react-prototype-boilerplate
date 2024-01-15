695b4bad6d92f8657fbefad5670bca1c1202d658
Nikita Chernov <drimchansky@gmail.com>
2024-01-15 21:20:57 +0400

    add postcss, stylelint, update files structure

--- src/components/hello/hello.tsx
+++ src/components/hello/hello.tsx
@@ -1,33 +1,32 @@
 import { useStore } from '@nanostores/react'
+import clsx from 'clsx'
 
 import { $counter, increaseCounter } from '../../app/store/counter'
 import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
-import './hello.css'
+import styles from './hello.module.css'
 
-function Hello() {
+export const Hello = () => {
   const counter = useStore($counter)
 
   return (
-    <>
+    <div className={styles.hello}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={clsx(styles.logo, styles.react)} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles.card}>
         <button onClick={increaseCounter}>count is {counter}</button>
         <p>
           Edit <code>src/components/hello/hello.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      <p className={styles['read-the-docs']}>Click on the Vite and React logos to learn more</p>
+    </div>
   )
 }
-
-export default Hello
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './components/hello/hello.tsx'
+import { Hello } from './components/hello'
 import './app/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>
 )
